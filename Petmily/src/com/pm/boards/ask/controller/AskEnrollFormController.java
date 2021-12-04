eddfa235877d128ed86bc144640941cb6a84d191
RE-Coder / Minji Kim <88353455+RE-CoderMJ@users.noreply.github.com>
2021-12-05 02:22:14 +0900

    Update AskEnrollFormController.java

--- Petmily/src/com/pm/boards/ask/controller/AskEnrollFormController.java
+++ Petmily/src/com/pm/boards/ask/controller/AskEnrollFormController.java
@@ -15,6 +15,7 @@ public class AskEnrollFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
+     * @see HttpServlet#HttpServlet()
      */
     public AskEnrollFormController() {
         super();
