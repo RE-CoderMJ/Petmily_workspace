a4a25662841a7e15e2b9d126e17b7ae14dd776fb
RE-Coder / Minji Kim <88353455+RE-CoderMJ@users.noreply.github.com>
2021-12-16 15:03:03 +0900

    Attachment vo 클래스 완성

--- Petmily/src/com/pm/common/model/vo/Attachment.java
+++ Petmily/src/com/pm/common/model/vo/Attachment.java
@@ -7,9 +7,97 @@ public class Attachment {
 	private int attachmentNo;
 	private String originName;
 	private String changeName;
-	private String filePath;
-	private int refLevel; // 게시판 번호 궁금해요:1, 중고거래:2, 찾고있어요:3
+	private int refBoardNo; // 게시판 번호 궁금해요:1, 중고거래:2, 찾고있어요:3
 	private int refNo; // 글번호
+	private String filePath;
 	private Date uploadDate;
 	private String status;
+	
+	public Attachment () {}
+
+	public Attachment(int attachmentNo, String originName, String changeName, int refBoardNo, int refNo,
+			String filePath, Date uploadDate, String status) {
+		super();
+		this.attachmentNo = attachmentNo;
+		this.originName = originName;
+		this.changeName = changeName;
+		this.refBoardNo = refBoardNo;
+		this.refNo = refNo;
+		this.filePath = filePath;
+		this.uploadDate = uploadDate;
+		this.status = status;
+	}
+
+	public int getAttachmentNo() {
+		return attachmentNo;
+	}
+
+	public void setAttachmentNo(int attachmentNo) {
+		this.attachmentNo = attachmentNo;
+	}
+
+	public String getOriginName() {
+		return originName;
+	}
+
+	public void setOriginName(String originName) {
+		this.originName = originName;
+	}
+
+	public String getChangeName() {
+		return changeName;
+	}
+
+	public void setChangeName(String changeName) {
+		this.changeName = changeName;
+	}
+
+	public int getRefBoardNo() {
+		return refBoardNo;
+	}
+
+	public void setRefBoardNo(int refBoardNo) {
+		this.refBoardNo = refBoardNo;
+	}
+
+	public int getRefNo() {
+		return refNo;
+	}
+
+	public void setRefNo(int refNo) {
+		this.refNo = refNo;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public Date getUploadDate() {
+		return uploadDate;
+	}
+
+	public void setUploadDate(Date uploadDate) {
+		this.uploadDate = uploadDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Attachment [attachmentNo=" + attachmentNo + ", originName=" + originName + ", changeName=" + changeName
+				+ ", refBoardNo=" + refBoardNo + ", refNo=" + refNo + ", filePath=" + filePath + ", uploadDate="
+				+ uploadDate + ", status=" + status + "]";
+	}
+	
+	
 }
