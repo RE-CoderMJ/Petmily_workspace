c19933c770f6fd77d8f19e514bcbb173bf7ac565
kwonhanna <90588452+kwonhanna@users.noreply.github.com>
2021-12-16 21:48:19 +0900

    상품 페이지

---
+++ Petmily/src/com/pm/shopping/controller/ShopProductController.java
@@ -0,0 +1,40 @@
+package com.pm.shopping.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShopProductController
+ */
+@WebServlet("/shop.product")
+public class ShopProductController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ShopProductController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("views/boards/shopping/product_page.jsp").forward(request, response);
+	}
+    
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
